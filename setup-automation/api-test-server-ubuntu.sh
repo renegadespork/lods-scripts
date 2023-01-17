30d9323bd7dc0ba570a55c312bb3721a7663bd60
Jesse <jessekilburn@gmail.com>
2023-01-17 13:22:39 -0800

    Update api-test-server-ubuntu.sh

--- setup-automation/api-test-server-ubuntu.sh
+++ setup-automation/api-test-server-ubuntu.sh
@@ -2,4 +2,4 @@ apt-get -y install docker &&
 apt-get -y install docker-compose &&
 useradd -rmg 103 docker &&
 mkdir /home/docker/compose &&
-wget -O /home/docker/compose https://raw.githubusercontent.com/renegadespork/lods-scripts/main/setup-automation/docker-compose.yml
+wget -O /home/docker/compose/docker-compose.yml https://raw.githubusercontent.com/renegadespork/lods-scripts/main/setup-automation/docker-compose.yml
